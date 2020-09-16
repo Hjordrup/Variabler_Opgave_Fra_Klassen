4fad2480ad21679044233ba8821d0cd9625ef2da
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-09-16 13:17:39 +0200

    Initial commit

---
+++ src/Variabler.java
@@ -0,0 +1,50 @@
+public class Variabler {
+
+    public static void main(String[] args) {
+        byte etTal = 126;
+        short etKortTal = 31000;
+        int etHelTal = 10;
+        long etLangtTal = 1000l;
+        float etTalMedFlyvendeDecimal = 9.55445f;
+        double etStortTal = 5656556;
+        boolean jaEllerNej = true;
+        char etBokstav = 'a';
+
+
+        System.out.println(etTal);
+        System.out.println(etKortTal);
+        System.out.println(etHelTal);
+        System.out.println(etLangtTal);
+        System.out.println(etTalMedFlyvendeDecimal);
+        System.out.println(etStortTal);
+        System.out.println(jaEllerNej);
+        System.out.println(etBokstav);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
